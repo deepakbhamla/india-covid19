0f7ca6f17a59398d478b6519bd6d9fea842424ad
Deepak Bhamla <dbhamla7@gmail.com>
2020-08-09 13:04:56 +0530

    Updates

---
+++ precache-manifest.941b85082aa5aa180757c7d7d78f636b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e5961ab54671d3af63f8f32aeef81e",
+    "url": "/india-covid19/index.html"
+  },
+  {
+    "revision": "c8340aa41ff0eb38af36",
+    "url": "/india-covid19/static/css/main.be212863.chunk.css"
+  },
+  {
+    "revision": "6e14b1622f76a65beef7",
+    "url": "/india-covid19/static/js/2.3013a7b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/india-covid19/static/js/2.3013a7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8340aa41ff0eb38af36",
+    "url": "/india-covid19/static/js/main.f73b81b0.chunk.js"
+  },
+  {
+    "revision": "4cae6ddf1917d2e19ac4",
+    "url": "/india-covid19/static/js/runtime-main.a0ed89a8.js"
+  }
+]);
